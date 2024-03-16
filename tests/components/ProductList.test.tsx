8d329ccc559d0da86d00364ff2e494569fd35195
maxymlyskov <raypember02@gmail.com>
2024-03-16 22:49:43 +0200

    Refactor using react query.

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -3,6 +3,7 @@ import { HttpResponse, delay, http } from 'msw'
 import ProductList from '../../src/components/ProductList'
 import { db } from '../mocks/db'
 import { server } from '../mocks/server'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 describe('ProductList', () => {
 
@@ -19,8 +20,20 @@ describe('ProductList', () => {
         db.product.deleteMany({ where: { id: { in: productIds } } })
     })
 
+    const renderComponent = () => {
+        const client = new QueryClient({
+            defaultOptions: {
+                queries: {
+                    retry: false
+                }
+            }
+        })
+
+        render(<QueryClientProvider client={client}><ProductList /></QueryClientProvider>)
+    }
+
     it('should render the list of products', async () => {
-        render(<ProductList />)
+        renderComponent()
 
         const items = await screen.findAllByRole('listitem')
         expect(items.length).toBeGreaterThan(0)
@@ -32,26 +45,26 @@ describe('ProductList', () => {
 
             return HttpResponse.json([])
         }))
-        render(<ProductList />)
+        renderComponent()
 
         const loading = await screen.findByText('Loading...')
         expect(loading).toBeInTheDocument()
     })
 
     it('should remove the loading message after products fetched', async () => {
-        render(<ProductList />)
+        renderComponent()
         await waitForElementToBeRemoved(() => screen.queryByText('Loading...'))
     })
 
     it('should remove the loading if data fetching failed', async () => {
         server.use(http.get('/products', () => HttpResponse.error()))
-        render(<ProductList />)
+        renderComponent()
         await waitForElementToBeRemoved(() => screen.queryByText('Loading...'))
     })
 
     it('should render an error message if no products', async () => {
         server.use(http.get('/products', () => HttpResponse.json([])))
-        render(<ProductList />)
+        renderComponent()
 
         const error = await screen.findByText(/no products/i)
         expect(error).toBeInTheDocument()
@@ -59,7 +72,7 @@ describe('ProductList', () => {
 
     it('should render an error message if the request fails', async () => {
         server.use(http.get('/products', () => HttpResponse.error()))
-        render(<ProductList />)
+        renderComponent()
 
         const error = await screen.findByText(/error/i)
         expect(error).toBeInTheDocument()
