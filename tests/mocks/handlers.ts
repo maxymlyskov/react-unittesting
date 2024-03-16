e5d2f2b6beaa30759df2cafa5fc19e948a4b3301
maxymlyskov <raypember02@gmail.com>
2024-03-16 19:48:40 +0200

    Add data mocking with db.

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,28 +1,7 @@
 import { http, HttpResponse } from 'msw'
 import { products } from './data'
+import { db } from './db'
 
 export const handlers = [
-    http.get('/categories', () => {
-        return HttpResponse.json([
-            { id: 1, name: 'Category 1' },
-            { id: 2, name: 'Category 2' },
-        ])
-    }),
-
-    http.get('/products', () => {
-        return HttpResponse.json(products)
-    }),
-
-    http.get('/products/:id', ({ params }) => {
-        const id = parseInt(params.id as string)
-        const product = products.find(p => p.id === id)
-
-        if (!product) {
-            return HttpResponse.json(null, { status: 404 })
-        }
-
-        return HttpResponse.json(product)
-    }),
-
-
+    ...db.product.toHandlers('rest')
 ]
