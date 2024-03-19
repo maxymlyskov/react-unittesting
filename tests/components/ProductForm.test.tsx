bb961007da1abbb02c28286c93128a544a91a7b4
maxymlyskov <raypember02@gmail.com>
2024-03-19 20:18:18 +0200

    Refactor ProductForm.

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -15,24 +15,45 @@ describe('ProductForm', () => {
         db.category.delete({ where: { id: { equals: category.id } } })
     })
 
+    const renderComponent = (product?: Product) => {
+        render(<ProductForm product={product} onSubmit={vi.fn()} />, { wrapper: AllProviders })
+
+
+        return {
+            waitForFormToLoad: () => waitForElementToBeRemoved(() => screen.getByText(/loading/i)),
+            getInputs: () => {
+                const nameInput = screen.getByPlaceholderText(/name/i)
+                const priceInput = screen.getByPlaceholderText(/price/i)
+                const categorySelect = screen.getByRole('combobox', { name: /category/i })
+
+                return { nameInput, priceInput, categorySelect }
+            }
+        }
+
+    }
+
     it('should render form fields', async () => {
-        render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProviders })
+        const { getInputs, waitForFormToLoad } = renderComponent()
 
-        await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
+        await waitForFormToLoad()
 
-        expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
-        expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument()
-        expect(screen.getByRole('combobox', { name: /category/i })).toBeInTheDocument()
+        const { nameInput, priceInput, categorySelect } = getInputs()
+
+        expect(nameInput).toBeInTheDocument()
+        expect(priceInput).toBeInTheDocument()
+        expect(categorySelect).toBeInTheDocument()
     })
 
     it('should populate form fields when editing a product', async () => {
-        const product: Product = { id: 1, name: 'Product 1', price: 10, categoryId: category.id }
-        render(<ProductForm product={product} onSubmit={vi.fn()} />, { wrapper: AllProviders })
+        const product: Product = { id: 1, name: 'Product 1', price: 100, categoryId: category.id }
+        const { getInputs, waitForFormToLoad } = renderComponent(product)
 
-        await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
+        await waitForFormToLoad()
 
-        expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name)
-        expect(screen.getByPlaceholderText(/price/i)).toHaveValue(product.price.toString())
-        expect(screen.getByRole('combobox', { name: /category/i })).toHaveTextContent(category.name)
+        const { nameInput, priceInput, categorySelect } = getInputs()
+
+        expect(nameInput).toHaveValue(product.name)
+        expect(priceInput).toHaveValue(product.price.toString())
+        expect(categorySelect).toHaveTextContent(category.name)
     })
 })
