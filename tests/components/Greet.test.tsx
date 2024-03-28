f993067c4db7fe888d3313db2544b3d634bb5c61
maxymlyskov <raypember02@gmail.com>
2024-03-28 21:45:21 +0200

    Add Router test.

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,20 +1,20 @@
-import Greet from '../../src/components/Greet'
-import { render, screen } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
+import Greet from "../../src/components/Greet";
 
-describe('Greet', () => {
-    it('should render Hello when render provided', () => {
-        render(<Greet name='Max' />)
+describe("Greet", () => {
+  it("should render Hello when render provided", () => {
+    render(<Greet name="Max" />);
 
-        const heading = screen.getByRole('heading')
-        expect(heading).toBeInTheDocument()
-        expect(heading).toHaveTextContent(/max/i)
-    })
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/max/i);
+  });
 
-    it('should render Login button when no name provided', () => {
-        render(<Greet />)
+  it("should render Login button when no name provided", () => {
+    render(<Greet />);
 
-        const button = screen.getByRole('button')
-        expect(button).toBeInTheDocument()
-        expect(button).toHaveTextContent(/login/i)
-    })
-}) 
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/login/i);
+  });
+});
