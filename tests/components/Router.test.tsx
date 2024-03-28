1aa7265c19320602dc3a702fc33c38a2d21d91e5
maxymlyskov <raypember02@gmail.com>
2024-03-28 21:55:00 +0200

    Add product details test.

--- tests/components/Router.test.tsx
+++ tests/components/Router.test.tsx
@@ -1,4 +1,6 @@
 import { screen } from "@testing-library/react";
+import { Product } from "../../src/entities";
+import { db } from "../mocks/db";
 import { navigateTo } from "../utils";
 
 describe("Router", () => {
@@ -13,5 +15,19 @@ describe("Router", () => {
     expect(
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
+  });
+  it("should render the product details page for /products/:id", async () => {
+    const product: Product = {
+      id: 1,
+      name: "Product 1",
+      price: 100,
+      categoryId: 1,
+    };
+    db.product.create(product);
+    navigateTo("/products/ " + product.id);
+
+    expect(await screen.findByRole("heading", { name: product.name }));
+
+    db.product.delete({ where: { id: { equals: product.id } } });
   });
 });
